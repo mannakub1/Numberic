0f6e88ab13f3c832654401a702eca35433fdd93f
Deklnwzaza <dietyboy@hotmail.com>
2015-11-29 01:10:42 +0700

    Create Main Activity

--- app/src/main/java/com/example/manny/numberic/MainActivity.java
+++ app/src/main/java/com/example/manny/numberic/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.manny.numberic;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.manny.numberic.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button startGame = (Button) findViewById(R.id.Start_button);
+        startGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(i);
+            }
+        });
+
+        Button highScore = (Button) findViewById(R.id.HighScore_button);
+        highScore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, HighScoreActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
