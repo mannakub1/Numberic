2eeffdcbe5d24d03a2bbafba9bb7c6725b4501b7
Deklnwzaza <dietyboy@hotmail.com>
2015-12-01 23:22:53 +0700

    Update UI

--- app/src/main/java/com/example/manny/numberic/MainActivity.java
+++ app/src/main/java/com/example/manny/numberic/MainActivity.java
@@ -1,19 +1,31 @@
 package com.example.manny.numberic;
 
+import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SimpleCursorAdapter;
 
 import com.example.manny.numberic.R;
+import com.example.manny.numberic.model.model;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String[] Level={"Easy", "Mediuun", "Hard"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         Button startGame = (Button) findViewById(R.id.Start_button);
         startGame.setOnClickListener(new View.OnClickListener() {
@@ -34,4 +46,30 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+    // For GameActivity
+    public void select_level(){
+        new AlertDialog.Builder(MainActivity.this)
+                .setTitle("Please Select Level")
+                .setItems(Level, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String select = Level[which];
+                        if(select.equals("Easy")) {
+
+                        }
+                        else if(select.equals("Medium")){
+
+                        }
+                        else{
+
+                        }
+            }
+        })
+                .create()
+                .show();
+    }
+
+
+
 }
